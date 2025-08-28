e625b73169a33c4bb5ca733906f2044047324c0b
ambatiweb <ambatiweb@gmail.com>
2025-08-28 12:40:47 +0530

    Initial Commit

---
+++ src/main/java/com/vcb/collab/SpringBootCollabApplication.java
@@ -0,0 +1,13 @@
+package com.vcb.collab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCollabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcb/collab/SpringBootCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcb.collab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
